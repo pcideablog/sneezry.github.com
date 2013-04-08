0c19f2be0a44d2088b17797308d370ca5241ed66
Sneezry <lizhe@lizhe.org>
2013-04-08 22:08:27 +0800

    13040801

---
+++ javascripts/getmd.js
@@ -0,0 +1,38 @@
+var fname = location.search ? location.search.substr(1) : (location.hash ? location.hash.substr(1) : 'defalut');
+var main = document.getElementById('main-content');
+var xmlhttp;
+
+loadXMLDoc(fname)
+
+function loadXMLDoc(url){
+	xmlhttp=null;
+	if (window.XMLHttpRequest){// code for IE7, Firefox, Opera, etc.
+		xmlhttp=new XMLHttpRequest();
+	}
+	else if (window.ActiveXObject){// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	if (xmlhttp!=null){
+		xmlhttp.onreadystatechange=state_Change;
+		xmlhttp.open("GET",url,true);
+		xmlhttp.send(null);
+	}
+	else{
+		alert("Your browser does not support XMLHTTP.");
+	}
+}
+
+function state_Change(){
+	if (xmlhttp.readyState==4){// 4 = "loaded"
+		if (xmlhttp.status==200){// 200 = "OK"
+			var converter = new Showdown.converter();
+			main.innerHTML = converter.makeHtml(xmlhttp.responseText);
+		}
+		else if(xmlhttp.status==404) {
+			main.innerHTML = '404';
+		}
+		else {
+			main.innerHTML = 'We meet a problem.';
+		}
+	}
+}
