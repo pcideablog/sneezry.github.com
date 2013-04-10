2b71c7c9a0502251686416dd3ef4071df502e3e6
Sneezry <lizhe@lizhe.org>
2013-04-10 12:45:57 +0800

    Error Code

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -62,7 +62,7 @@ function state_Change(){
 			content.innerHTML = '<img src="/images/404.jpg" />';
 		}
 		else {
-			content.innerHTML = 'We meet a problem.';
+			content.innerHTML = '<h2>We meet a problem when try to handle ' + path + ' (Err: ' + xmlhttp.status + ').<h2>';
 		}
 	}
 }
