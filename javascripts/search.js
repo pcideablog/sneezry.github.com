651a8e3675ccf70b4825a7bc4b39b74e39472290
Sneezry <lizhe@lizhe.org>
2013-04-12 11:11:35 +0800

    search

--- javascripts/search.js
+++ javascripts/search.js
@@ -28,6 +28,7 @@ function search(q){
 
 function searchlist(list){
 	listTotal = list.data.length;
+	currentTotal = 0;
 	for(var i = list.data.length; i > 0; i--){
 		if(list.data[i-1].name.toLowerCase().indexOf(kw.toLowerCase()) != -1){
 			content.innerHTML += '<h2><a href="/#!/' + list.data[i-1].name.replace(/-/g, '/') + '">' + list.data[i-1].name.split('-')[list.data[i-1].name.split('-').length-1] + '</a></h2>';
