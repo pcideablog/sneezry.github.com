3f9ba61c876d09a1ab935bc53f1e111cb1cc9320
Sneezry <lizhe@lizhe.org>
2013-04-14 23:29:38 +0800

    title

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -93,7 +93,7 @@ function loadXMLDoc(url){
 function showpost(path){
 	//window.history.pushState(null, path.substr(1).split('/')[path.substr(1).split('/').length-1] + ' - Sneezry', path);
 	var url = location.protocol + '//' + location.hostname + '/md/' + path.substr(1).replace(/\//g, '-');
-	document.title = path.substr(1).split('/')[path.substr(1).split('/').length-1] + ' - Sneezry';
+	document.title = path.substr(1).split('/')[path.substr(1).split('/').length-1].replace(/_/g, ' ') + ' - Sneezry';
 	pdate = path.substr(1).split('/')[0]+'-'+path.substr(1).split('/')[1]+'-'+path.substr(1).split('/')[2];
 	loadXMLDoc(url);
 }
