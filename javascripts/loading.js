cdb5f8955205b227cfcb09362a1c3b743e228277
Sneezry <lizhe@lizhe.org>
2013-05-22 11:56:24 +0800

    loading

--- javascripts/loading.js
+++
@@ -1,7 +0,0 @@
-((function(){
-	var el = document.createElement('link');
-	el.rel = 'stylesheet';
-	el.type = 'text/css';
-	el.href= '/stylesheets/loading.css';
-	document.getElementsByTagName('head')[0].appendChild(el);
-})());
