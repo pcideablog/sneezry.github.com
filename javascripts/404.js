b7b5239a0eade6e997d69124acd3f77e44ed0150
Sneezry <lizhe@lizhe.org>
2013-06-05 11:33:25 +0800

    rss

--- javascripts/404.js
+++ javascripts/404.js
@@ -1,5 +1,5 @@
 var path = location.pathname;
-if(path == '/rss' || path == '/feed' path == '/atom'){
+if(path == '/rss' || path == '/feed' || path == '/atom'){
 	location.href = 'http://feeds.feedburner.com/Sneezry';
 }
 else{
