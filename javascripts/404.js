b60e7f532e84797ff8985e55bb7937f091740e07
Sneezry <lizhe@lizhe.org>
2013-07-04 09:08:15 +0800

    Update 404.js

--- javascripts/404.js
+++ javascripts/404.js
@@ -1,5 +1,5 @@
 var path = location.pathname;
-if(path == '/rss' || path == '/feed' || path == '/atom'){
+if(path == '/rss' || path == '/rss/' || path == '/feed' || path == '/feed/' || path == '/atom' || path == '/atom/'){
 	location.href = 'http://feeds.feedburner.com/Sneezry';
 }
 else{
