b4d06915fb9e586926d14efd9f675b60d59c205c
Sneezry <lizhe@lizhe.org>
2013-11-04 12:50:13 +0800

    Update getmd.js

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -1,6 +1,6 @@
 var path = decodeURIComponent(location.hash.substr(2));
-if(location.hash.search.substr(1,18)=='_escaped_fragment_'){
-	path = decodeURIComponent(location.hash.search.substr(19));
+if(location.hash.search.substr(1,19)=='_escaped_fragment_='){
+	path = decodeURIComponent(location.hash.search.substr(20));
 }
 if(path == '/'){path = ''; window.history.pushState(null, '', '/');}
 else if(path){window.history.pushState(null, '', '/#!'+path);}
