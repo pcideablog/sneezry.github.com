5dc69640920b681c003b4d5008746bdfc0ca0fb1
Sneezry <lizhe@lizhe.org>
2013-11-05 15:06:28 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -30,3 +30,52 @@ function goToTop(acceleration, time) {
 		window.setTimeout(invokeFunction, time);
 	}
 }
+
+var SmoothScroll = function (win, opt) {  
+    //操作对象  
+    this.win = win;  
+    //每次滚动位移  
+    this.step = opt ? opt.step || 180 : 180;  
+    //缓动系数  
+    this.f = opt ? opt.f || 0.1 : 0.1;  
+    this.interval = 10;  
+    this.intervalID = null;  
+    this.isFF = navigator.userAgent.toLowerCase().indexOf("firefox") >= 0;  
+    this.upOrDown = "";  
+    this.init();  
+}
+
+SmoothScroll.prototype = {  
+    init: function () {  
+        var _this = this;  
+        if (_this.isFF) {  
+            _this.win.addEventListener('DOMMouseScroll', function (e) {  
+                _this.upOrDown = e.detail < 0 ? "up" : "down";  
+                _this.scrollHander();  
+                e.preventDefault();  
+            }, false);  
+        } else {  
+            _this.win.onmousewheel = function (e) {  
+                e = e || window.event;  
+                _this.upOrDown = e.wheelDelta > 0 ? "up" : "down";  
+                _this.scrollHander();  
+                e.returnValue = false;  
+            }  
+        }  
+    },
+    scrollHander: function () {  
+        var _this = this;  
+        clearInterval(_this.intervalID);  
+        //目标位置  
+        var tar = _this.win.scrollTop + _this.step * (_this.upOrDown == "up" ? -1 : 1);  
+        _this.intervalID = setInterval(function () {  
+            //缓动  
+            _this.win.scrollTop += (tar - _this.win.scrollTop) * _this.f;  
+            if (tar == _this.win.scrollTop) {  
+                clearInterval(_this.intervalID);  
+            }  
+        }, _this.interval);  
+    }  
+}
+
+new SmoothScroll(document.getElementsByTagName('body')[0], {f:0.2});
