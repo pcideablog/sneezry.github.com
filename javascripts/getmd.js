5c3216813e3fa0734f12eaae8dafc9d6262d7e94
Sneezry <lizhe@lizhe.org>
2013-11-07 09:42:48 +0800

    page

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -131,15 +131,15 @@ function showlist(list){
 		page = Math.ceil(list.data.length/20);
 		window.history.replaceState(null, '', '/#!/page/'+page);
 	}
-	for(var i = list.data.length-(page-1)*20; i > 0; i--){
+	for(var i = list.data.length-(page-1)*20; i > list.data.length-(page-2)*20; i--){
 		console.log(i);
 		console.log(txt);
 		txt += '<postlist><a href="/#!/' + list.data[i-1].name.replace(/-/g, '/') + '">' + list.data[i-1].name.split('-')[list.data[i-1].name.split('-').length-1].replace(/_/g, ' ') + '</a><div class="post_info"><span class="post_date">Posted at '+list.data[i-1].name.split('-')[0]+'-'+list.data[i-1].name.split('-')[1]+'-'+list.data[i-1].name.split('-')[2]+'</span><span class="disqus_count"><a href="' + hostbase + '/' + encodePath(list.data[i-1].name) + '#disqus_thread"></a></span></div></postlist>';
 	}
-	if(page>1 && page*20>=list.data.length){
+	if(page==1 && page*20<list.data.length){
 		txt += '<postlist><a class="prev_page" href="/#!/page/'+(page+1)+'">←之前的文章</a><div style="clear:both"></div></postlist>';
 	}
-	else if(page==1 && page*20<list.data.length){
+	else if(page>1 && page*20>=list.data.length){
 		txt += '<postlist><a class="next_page" href="/#!/page/'+(page-1)+'">更新的文章→</a><div style="clear:both"></div></postlist>';
 	}
 	else if(page>1 && page*20<list.data.length){
