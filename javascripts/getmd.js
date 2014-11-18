35abd4c108dd32e9eb2a244f3100836d47eaa701
Li Zhe <lizhe@lizhe.org>
2014-11-18 17:20:02 +0800

    Update getmd.js

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -115,6 +115,10 @@ function loadXMLDoc(url){
 						content.innerHTML = blog_text;
 						LatexIT.render('*',false);
 						blog_text = content.innerHTML;
+						blog_text = blog_text.replace(/&lt;/g, '<');
+						blog_text = blog_text.replace(/&gt;/g, '>');
+						blog_text = blog_text.replace(/&amp;/g, '&');
+						blog_text = blog_text.replace(/&nbsp;/g, ' ');
 						var converter = new Showdown.converter();
 						content.innerHTML = '<div id="content_inner"><div id="back_home"><a href="/" onclick="home();return false;">'+sitetitle+'</a><span>&nbsp;›&nbsp;</span></div><div id="post_title">' + decodeUtf8(getPostName(path)) + (encoded?Base64.decode('PHN1cCBzdHlsZT0iZm9udC1zaXplOjAuNWVtO3ZlcnRpY2FsLWFsaWduOiBzdXBlcjsiIHRpdGxlPSLmraTmlofnq6Dlt7Looqvph43mlrDnvJbnoIHku6XourLpgb/lrqHmn6UiPuKYmuiiq+e8lueggeeahOWGheWuuTwvc3VwPg=='):'')  + '<div id="shorturl">Checking short URL for this post...</div></div>' + converter.makeHtml(blog_text) + '<div class="date"><span>S</span>Posted at ' + pdate + '</div></div>';
 						content.style.display = 'block';
